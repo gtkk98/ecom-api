6eafafdf9454bf7163ca9f94096b42760ed5f43f
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-21 12:30:37 +0530

    Product and User table has been created.

---
+++ src/main/java/com/ecom/ecom_api/model/User.java
@@ -0,0 +1,28 @@
+package com.ecom.ecom_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+}
