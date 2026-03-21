fb654d4b3f7494ec478ebf379a32cb9d8907ae58
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-21 13:30:46 +0530

    Order item table has been created.

---
+++ src/main/java/com/ecom/ecom_api/model/OrderItem.java
@@ -0,0 +1,30 @@
+package com.ecom.ecom_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
