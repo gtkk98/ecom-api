fda84d6a3cd5e894e10d2a53ccb9403c7d7b2c86
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-21 12:59:51 +0530

    Order table and Enum Order Status has been created.

---
+++ src/main/java/com/ecom/ecom_api/model/Order.java
@@ -0,0 +1,40 @@
+package com.ecom.ecom_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/ecom/ecom_api/model/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.ecom.ecom_api.model;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+}
