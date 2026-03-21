70c3ac042dd5cf3125a54a786960639463029202
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-21 18:09:26 +0530

    Product service file completed.

---
+++ src/main/java/com/ecom/ecom_api/service/ProductService.java
@@ -0,0 +1,55 @@
+package com.ecom.ecom_api.service;
+
+import com.ecom.ecom_api.model.Product;
+import com.ecom.ecom_api.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductId(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+    }
+
+    public List<Product> searchProducts(String name) {
+        return productRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Product existing =  getProductId(updatedProduct.getId());
+        existing.setName(updatedProduct.getName());
+        existing.setDescription(updatedProduct.getDescription());
+        existing.setPrice(updatedProduct.getPrice());
+        existing.setStock(updatedProduct.getStock());
+        return productRepository.save(existing);
+    }
+
+    public void deleteProduct(Long id) {
+        getProductId(id); // Ensure product exists before deleting
+        productRepository.deleteById(id);
+    }
+
+    public Product reduceStock(Long productId, int quantity) {
+        Product product = getProductId(productId);
+        if (product.getStock() < quantity) {
+            throw new RuntimeException("Insufficient stock for product id: " + productId);
+        }
+        product.setStock(product.getStock() - quantity);
+        return productRepository.save(product);
+    }
+}
