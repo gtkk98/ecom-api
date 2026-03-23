cad86097669c889d2550af38b06b9f2d18948c53
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-23 13:05:15 +0530

    Auth service user register for JWT function build and Login request bug fixed.

--- src/main/java/com/ecom/ecom_api/service/AuthService.java
+++ src/main/java/com/ecom/ecom_api/service/AuthService.java
@@ -1,11 +1,14 @@
 package com.ecom.ecom_api.service;
 
+import com.ecom.ecom_api.dto.request.LoginRequest;
 import com.ecom.ecom_api.dto.request.RegisterRequest;
 import com.ecom.ecom_api.dto.response.AuthResponse;
 import com.ecom.ecom_api.model.User;
 import com.ecom.ecom_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +20,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
-    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomUserDetailsService userDetailsService;
 
     public AuthResponse register(RegisterRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
@@ -30,5 +33,27 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         userRepository.save(user);
+
+        UserDetails userDetails = userDetailsService
+                .loadUserByUsername(request.getEmail());
+        String token = jwtService.generateToken(userDetails);
+
+        return new AuthResponse(token, user.getEmail(), user.getName());
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails userDetails = userDetailsService
+                .loadUserByUsername(request.getEmail());
+        String token = jwtService.generateToken(userDetails);
+
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        return new AuthResponse(token, user.getEmail(), user.getName());
     }
 }
