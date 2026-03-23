9d8603906f644ddcd30ab52039a6f302572a9c01
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-23 11:00:50 +0530

    Customer user details service build for Spring Security needs to know how to load a user by email.

---
+++ src/main/java/com/ecom/ecom_api/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.ecom.ecom_api.service;
+
+import com.ecom.ecom_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+        throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .roles("USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "User not found with email: " + email));
+
+    }
+}
